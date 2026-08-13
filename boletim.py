32c124db6ec552ea8fc107910d4479e59a1f649f
Rhasller <rhasllerfelipem@gmail.com>
2026-08-12 21:11:17 -0300

    Create boletim.py

---
+++ boletim.py
@@ -0,0 +1,18 @@
+print ("Boletim De Lançamento de Notas")
+
+nome = input("Nome do aluno: ")
+
+curso = input("Curso: ")
+
+    
+disciplina = input ("Disciplina: ")
+    
+nota = float (input ("Nota: "))
+
+if nota >59 and nota <101:
+    print ("Está APROVADO👍")
+
+elif nota <20:
+    print("Está REPROVADO")
+else:
+    print ("EXAME FINAL, SEGUNDA VIDA NO DARK SOULS")
